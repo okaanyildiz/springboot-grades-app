ae3feab8fbdef0b7a51211d89fd639470807042f
Ozi <oguzkaanyildiz@mac-ozi.local>
2022-12-28 10:24:56 +0100

    Refactor with GradeRepository

--- grades/src/main/java/com/ltp/gradesubmission/controller/GradeController.java
+++ grades/src/main/java/com/ltp/gradesubmission/controller/GradeController.java
@@ -1,7 +1,4 @@
-package com.ltp.gradesubmission;
-
-import java.util.ArrayList;
-import java.util.List;
+package com.ltp.gradesubmission.controller;
 
 import javax.validation.Valid;
 
@@ -12,44 +9,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ltp.gradesubmission.Constant;
+import com.ltp.gradesubmission.Grade;
+import com.ltp.gradesubmission.repository.GradeRepository;
+
 @Controller
 public class GradeController {
 
-    List<Grade> studentGrades = new ArrayList<>();
+    GradeRepository gradeRepository = new GradeRepository();
 
     @GetMapping("/")
     public String gradeForm(Model model, @RequestParam(required = false) String id) {
         int index = getGradeIndex(id);
         model.addAttribute("grade",
-                index == Constant.NOT_FOUND ? new Grade() : studentGrades.get(index));
+                index == Constant.NOT_FOUND ? new Grade() : gradeRepository.getGrades().get(index));
         return "form";
     }
 
     @PostMapping("/handleSubmit")
     public String submitForm(@Valid Grade grade, BindingResult result) {
-        System.out.println("Has errors?: " + result.hasErrors());
 
         if (result.hasErrors())
             return "form";
 
         int index = getGradeIndex(grade.getId());
         if (index == Constant.NOT_FOUND) {
-            studentGrades.add(grade);
+            gradeRepository.addGrade(grade);
+            ;
         } else {
-            studentGrades.set(index, grade);
+            gradeRepository.updateGrade(grade, index);
         }
         return "redirect:/grades";
     }
 
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        model.addAttribute("grades", studentGrades);
+        model.addAttribute("grades", gradeRepository.getGrades());
         return "grades";
     }
 
     public Integer getGradeIndex(String id) {
-        for (int i = 0; i < studentGrades.size(); i++) {
-            if (studentGrades.get(i).getId().equals(id))
+        for (int i = 0; i < gradeRepository.getGrades().size(); i++) {
+            if (gradeRepository.getGrades().get(i).getId().equals(id))
                 return i;
         }
         return Constant.NOT_FOUND;
